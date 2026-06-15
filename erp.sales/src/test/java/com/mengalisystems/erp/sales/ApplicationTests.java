b059591e378e4351d4e98c85e0920a086a6c52c4
Cauan Mengali <cauanmengali10@gmail.com>
2026-06-15 19:49:57 -0300

    commit inicial

---
+++ erp.sales/src/test/java/com/mengalisystems/erp/sales/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mengalisystems.erp.sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
